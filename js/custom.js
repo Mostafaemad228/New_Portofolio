07d7139119cb9d476518886d208f52d1e21c20c7
Mostafaemad228 <42499279+Mostafaemad228@users.noreply.github.com>
2023-02-26 19:05:18 +0200

    commit

--- js/custom.js
+++ js/custom.js
@@ -1,6 +1,6 @@
 
   
-    // ? menu
+// ! defining 
 const body = document.getElementsByTagName('body')[0];
 let toggler = document.querySelector(".MENU_toggler");
 let nav = document.querySelector("nav");
@@ -26,10 +26,13 @@ let toggle_style_switcher = document.querySelector(".toggle-style-switcher");
 let style_switcher = document.querySelector(".style-switcher");
 let root = document.querySelector(':root');
 let darkmood_img = document.querySelector('.darkmood_img');
-let skills = document.querySelector('.skills');
+// let skills = document.querySelector('.skills');
+let Skills_container = document.querySelector('.Skills_container');
+let projects_container = document.querySelector('.projects_container');
 
 
-// ? ///////////////// menue togller ////////////////
+
+// ! ///////////////// menue togller ////////////////
 toggler.onclick = function(e) {
   nav.classList.toggle("open");
   bars.forEach(el => {
@@ -37,7 +40,7 @@ toggler.onclick = function(e) {
   });
 }
 
-// ? //////////////////// change menue togller on open and close /////////////////////////////
+// ! //////////////////// change menue togller on open and close /////////////////////////////
 nav.onclick = function (e) {
   nav.classList.toggle("open");
   bars.forEach(el => {
@@ -46,7 +49,7 @@ nav.onclick = function (e) {
   NavTitle.textContent = "Hello";
 }
 
-// ? //////////////////// close the nav bar on click any navlink  /////////////////////////////
+// ! //////////////////// close the nav bar on click any navlink  /////////////////////////////
 navlink.forEach(el => {
   el.addEventListener("click", function () {
     setTimeout(() => {
@@ -57,7 +60,7 @@ navlink.forEach(el => {
   })
 });
 
-// ? /////////////////////// set nav link text in another div on hover //////////////////////////
+// ! /////////////////////// set nav link text in another div on hover //////////////////////////
 
 navlink.forEach(el => {
   el.addEventListener("mouseenter", function () {
@@ -65,7 +68,7 @@ navlink.forEach(el => {
   })
 });
 
-// ? ////////////////close And close beaf Div/////////////////////
+// ! ////////////////close And close beaf Div/////////////////////
 //* open breaf div
 Wave.addEventListener("click", function () {
   My_breaf.style.display="block"
@@ -78,7 +81,7 @@ Close_Breaf.addEventListener("click", function () {
 });
 
 
-// ? ////////////////////// chang my photos    ///////////////////////////
+// ! ////////////////////// chang my photos    ///////////////////////////
 smallPhoto.forEach(el => {
   el.addEventListener("click", function () {
     bigPhoto.setAttribute("src", this.getAttribute('src'))
@@ -86,7 +89,7 @@ smallPhoto.forEach(el => {
 });
 
 
-// ? /////////////////////  lunch rocket (go to up)   ///////////////////////////
+// ! /////////////////////  lunch rocket (go to up)   ///////////////////////////
 
 to_up.onclick = function (e) {
 
@@ -99,14 +102,7 @@ to_up.onclick = function (e) {
 };
 
 
-
-
-
-
-
-
-// ? /////////////////////  change div to show personal information
-  // my photos   ///////////////////////////
+// ! ///////////// change div to show personal information ///////////
 
 meBtn.onclick = function (e) {
   personalData.style.display = "none";
@@ -120,9 +116,7 @@ Personal_Btn.onclick = function (e) {
 };
 
 
-// ? //////////////////  click sound effect ///////////////////////////////
-// ? //////////////////  click sound effect ///////////////////////////////
-// ? //////////////////  click sound effect ///////////////////////////////
+// ! //////////////////  click sound effect ///////////////////////////////
 
 
 music_img.onclick = function (e) {
@@ -142,9 +136,7 @@ music_img.onclick = function (e) {
 
 
 
-// ? ////////////////// open and close Style control ///////////////////////////////
-// ? ////////////////// open and close Style control ///////////////////////////////
-// ? ////////////////// open and close Style control ///////////////////////////////
+// ! ////////////////// open and close Style control ///////////////////////////////
 
 
 toggle_style_switcher.addEventListener("click", () => {
@@ -152,9 +144,7 @@ toggle_style_switcher.addEventListener("click", () => {
 });
 
 
-// ? //////////////////  dark mood system ///////////////////////////////
-// ? //////////////////  dark mood system ///////////////////////////////
-// ? //////////////////  dark mood system ///////////////////////////////
+// ! //////////////////  dark mood system ///////////////////////////////
 
 function setLightMode() {
   root.style.setProperty('--main-color', '#4a3b95');
@@ -171,9 +161,6 @@ function setLightMode() {
   root.style.setProperty('--home-background', 'url("../images/download (1).png") no-repeat center');
   root.style.setProperty('--projects-background', 'url("../images/download (1).png") no-repeat center');
   root.style.setProperty('--timeline-background', 'url("../images/6.webp") no-repeat center');
-  // skills.removeAttribute('data-vide-bg');
-  // skills.setAttribute('data-vide-bg', 'images/video/projects.webm');
-
 }
       
 
@@ -220,37 +207,161 @@ darkmood_img.addEventListener('click', toggleMood);
 
 
 
-// ? //////////////////  0000 ///////////////////////////////
-// ? //////////////////  0000 ///////////////////////////////
-// ? //////////////////  0000 ///////////////////////////////
+// ! ////////////////// display Items Skills section ///////////////////////////////
 
-// document.onkeyup = function (e) {
-//   if (e.key === "Escape") {
-//     nav.classList.remove("open");
-//   bars.forEach(bar => {
-//     bar.classList.toggle("allWidth");
-//   });
+const skills_arr = [
+  {
+    Skill_name:"HTML",
+    Skill_src: "images/skills/html.png",
+  },
+  {
+    Skill_name: "CSS",
+    Skill_src: "images/skills/css.png",
+  },
+  {
+    Skill_name: "JAVASCRIPT",
+    Skill_src: "images/skills/js.png",
+  },
+  {
+    Skill_name: "BOOTSTRAP",
+    Skill_src: "images/skills/bootstrap.png",
+  },
+  {
+    Skill_name: "SASS",
+    Skill_src: "images/skills/sass.png",
+  },
+  {
+    Skill_name: "JSON",
+    Skill_src: "images/skills/json.png",
+  },
+  {
+    Skill_name: "RESTFULL API",
+    Skill_src: "images/skills/restfull.png",
+  },
+  {
+    Skill_name: "ES6",
+    Skill_src: "images/skills/ES6.png",
+  },
+  {
+    Skill_name: "REACT JS",
+    Skill_src: "images/skills/react.png",
+  },
+  {
+    Skill_name: "REDUX",
+    Skill_src: "images/skills/redux.png",
+  },
+  {
+    Skill_name: "GITHUB",
+    Skill_src: "images/skills/github-mark.png",
+  },
+  {
+    Skill_name: "NPM",
+    Skill_src: "images/skills/npm.png",
+  },
+  {
+    Skill_name: "VSCode",
+    Skill_src: "images/skills/vscode.png",
+  },
+
+]
 
 
-//   }
-// }
+// let Skills_container = document.querySelector('.Skills_container');
+function displaySkillsItems() {  
+  Skills_container.innerHTML = ``
+  skills_arr.forEach((item) => {
+    let Counter = 0;
+    Skills_container.innerHTML += `
+            <div class="col-lg-3 col-md-3 col-sm-6 col-xs-12 p-2" data-aos="fade-in" data-aos-delay="${200}" data-aos-duration="1000">
+                <div class=" p-2 skill_container">
+                    <img src="${item.Skill_src}" class="img-fluid"   alt="">
+                    <span>${item.Skill_name}</span>
+                </div>
+            </div>`
+  });
+
+
+  console.log("Skills Data has been displayed in html showed");
+}
+
+
+displaySkillsItems();
 
 
 
 
-  // /////////////////////////////////////////
-  // window.addEventListener("scroll" , function () {    
-  //   var navbar =  document.querySelector("nav");
-  //   navbar.classList.toggle("sticky",window.scrollY >100);
-  // })
-  // ///////////////////////////////////////////////////////////// ///////////////////////
-
-// ////////////////////////// /////////////////////// ///////////////////////
 
 
-// ///////////////////////////////////////////////////////////////////////////
 
- 
+
+
+// ! ////////////////// display Items projects section ///////////////////////////////
+projects_data = [
+  {
+    project_img:"images/projects/cars.png",
+    project_demo_link:"https://mostafaemad228.github.io/cars/",
+    project_code_link:"https://github.com/Mostafaemad228/cars",
+  },
+  {
+    project_img: "images/projects/Glowing.png",
+    project_demo_link: "https://mostafaemad228.github.io/makeup-glowing-website/",
+    project_code_link: "https://github.com/Mostafaemad228/makeup-glowing-website",
+  },
+  {
+    project_img: "images/projects/Coffee.png",
+    project_demo_link: "https://mostafaemad228.github.io/coffe-website/",
+    project_code_link: "https://github.com/Mostafaemad228/coffe-website",
+  },
+  {
+    project_img: "images/projects/calculator.PNG",
+    project_demo_link: "https://mostafaemad228.github.io/calculator/",
+    project_code_link: "https://github.com/Mostafaemad228/calculator",
+  },
+  {
+    project_img: "images/projects/age.PNG",
+    project_demo_link: "https://mostafaemad228.github.io/Calculate_Age-app/",
+    project_code_link: "https://github.com/Mostafaemad228/Calculate_Age-app",
+  },
+  {
+    project_img: "images/projects/azkar.PNG",
+    project_demo_link: "https://mostafaemad228.github.io/Azkar/",
+    project_code_link: "https://github.com/Mostafaemad228/Azkar",
+  },
+];
+
+
+let c = 1 ;
+let dur = 200;
+
+function displayProjectsItems() {
+  projects_container.innerHTML = ``
+  for (let i = 0; i < projects_data.length; i++) {
+    projects_container.innerHTML +=`
+                  <div class="col-lg-4 p-3 mt-5 mb-3 text-center " data-aos="fade-in" data-aos-delay="${dur+200}" data-aos-duration="1400" >
+                         <h3 class="Project_num"> ${c++} </h3>
+                         <div class="col-lg-12 p-0  project  ">
+                            <div class="Project_inner shadow ">
+                                <div class="image_container" style="background-image: url('${projects_data[i].project_img}')"> </div> 
+                                <div class="overlay text-light d-flex justify-content-center align-items-center flex-row">
+                                    <a href="${projects_data[i].project_demo_link}" target="_blank">
+                                        <button class="Projects_btns">  <span> Demo 🔥  </span> </button>
+                                    </a>
+                                    <a href="${projects_data[i].project_code_link}" target="_blank">
+                                        <button class="Projects_btns">  <span> code 💻</span>  </button> 
+                                    </a> 
+                                </div>
+                            </div> 
+                                       
+                        </div>
+                    </div>`
+  
+}
+  console.log("Skills Data has been displayed in html showed");
+}
+
+displayProjectsItems();
+
+
   
 
   
